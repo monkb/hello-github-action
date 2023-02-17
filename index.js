3d5ae71d1a9cff0518bc1af394817f78520ec3c8
monkbangg <monkbangg@gmail.com>
2023-02-17 12:53:48 +0900

    octokit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as core from '@actions/core';
 
 import {debug} from "@actions/core";
+import {Octokit} from "@octokit/rest";
 
 async function run(){
   const token = process.env.GITHUB_TOKEN
