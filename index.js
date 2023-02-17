981c4f078addb0ad9e0750fb72875e45b7e75cf5
monkbangg <monkbangg@gmail.com>
2023-02-17 13:02:11 +0900

    github payload

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 import {debug} from "@actions/core";
 import {Octokit} from "@octokit/rest";
@@ -11,9 +12,17 @@ async function run(){
   debug('Hello World');
   debug(token);
 
+  const {payload} = github.context;
+
   const octokit = new Octokit({
     auth: token
   })
+
+  const owner = payload.repository.owner.login;
+  const repo = payload.repository.name;
+  const pr = payload.pull_request.number;
+
+  debug(`owner: ${owner} repo: ${repo} pr: ${pr}`);
 }
 
 (async ()=>{
